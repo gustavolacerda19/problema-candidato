1aae4f43677daccb18e5838ce9558ff3cc3438c4
Gustavo Lacerda <gustavolacerda655@gmail.com>
2020-05-13 23:20:54 -0300

    Leitura da escolaridade

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int escolaridade;
+
+    cout << "Qual sua escolaridade?" << endl;
+    cout << "1) Ensino Fundamental" << endl;
+    cout << "2) Ensino Medio" << endl;
+    cout << "3) Ensino Superior" << endl;
+    cout << "4) Pos-graduação" << endl;
+    cout << "Digite uma opcao: ";
+    cin >> escolaridade;
+
     return 0;
 }
